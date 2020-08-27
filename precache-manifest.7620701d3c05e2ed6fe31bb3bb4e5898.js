0e5c9884aa78da4558a93bb6077f91aa5e30fd45
Mikhail Chekov <chekoff@list.ru>
2020-08-27 15:18:39 +0300

    Updates

---
+++ precache-manifest.7620701d3c05e2ed6fe31bb3bb4e5898.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ad4d225e0af592d16dd44839bbeafb",
+    "url": "/kitties-food-choising-app/index.html"
+  },
+  {
+    "revision": "c713be851cd8d9a2c9e2",
+    "url": "/kitties-food-choising-app/static/css/main.45634226.chunk.css"
+  },
+  {
+    "revision": "3bef7a6507f43800ac3e",
+    "url": "/kitties-food-choising-app/static/js/2.f702ef9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kitties-food-choising-app/static/js/2.f702ef9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c713be851cd8d9a2c9e2",
+    "url": "/kitties-food-choising-app/static/js/main.c67fd22b.chunk.js"
+  },
+  {
+    "revision": "6e494e017a1b314f0406",
+    "url": "/kitties-food-choising-app/static/js/runtime-main.b252f15a.js"
+  },
+  {
+    "revision": "41b04bda61bbca902ee368f1afa15bb5",
+    "url": "/kitties-food-choising-app/static/media/Exo20-Thin.41b04bda.woff"
+  },
+  {
+    "revision": "c61c342ecb5413628f4b08b8fa3d651f",
+    "url": "/kitties-food-choising-app/static/media/Exo20-Thin.c61c342e.woff2"
+  },
+  {
+    "revision": "a671f1409f271a57fae059f5eeb3b4cd",
+    "url": "/kitties-food-choising-app/static/media/TrebuchetMS-Bold.a671f140.woff2"
+  },
+  {
+    "revision": "d0925a8d49c12c1556bbe95d17143a7b",
+    "url": "/kitties-food-choising-app/static/media/TrebuchetMS-Bold.d0925a8d.woff"
+  },
+  {
+    "revision": "3c2cf93f3ef115976f9f3115dc643c78",
+    "url": "/kitties-food-choising-app/static/media/TrebuchetMS.3c2cf93f.woff"
+  },
+  {
+    "revision": "877f72e1d3db01548f1fef12eb5ace17",
+    "url": "/kitties-food-choising-app/static/media/TrebuchetMS.877f72e1.woff2"
+  },
+  {
+    "revision": "8ec4b0d62fa09964167eb9295a4da8c8",
+    "url": "/kitties-food-choising-app/static/media/kitty.8ec4b0d6.png"
+  },
+  {
+    "revision": "f782a444c9877f04dce834988e8838cb",
+    "url": "/kitties-food-choising-app/static/media/pattern.f782a444.png"
+  }
+]);
